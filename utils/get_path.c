8e8047fefe9b5897d9641147c919a9d4d4d1ff54
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-24 23:57:41 +0000

    Create get_path.c

---
+++ utils/get_path.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 21:55:54 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/03/24 21:56:18 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+char	*get_path(char *cmd)
+{
+	char	**dirs;
+	size_t	i;
+	char	*tmp;
+	char	*res;
+
+	dirs = get_path_dirs();
+	i = -1;
+	while (dirs[++ i])
+	{
+		tmp = dirs[i];
+		dirs[i] = ft_strjoin(dirs[i], cmd, 2);
+		if((access(dirs[i], F_OK) == 0) && (access(dirs[i], X_OK) != -1))
+		{
+			res = ft_strdup(dirs[i]);
+			return (free(tmp), free_2d(dirs), res);
+		}
+		free(tmp);
+	}
+	free_2d(dirs);
+	return (NULL);
+}
