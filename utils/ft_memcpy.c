56d8a79f659a928baee29dffad120e234ef851e1
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-24 23:57:54 +0000

    Create ft_memcpy.c

---
+++ utils/ft_memcpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/24 20:46:25 by yel-hadd          #+#    #+#             */
+/*   Updated: 2023/03/24 20:47:22 by yel-hadd         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../pipex.h"
+
+void	*ft_memcpy(void *dst, void *src, size_t n)
+{
+	void	*dstcpy;
+
+	dstcpy = dst;
+	if (!dst && !src)
+		return (dstcpy);
+	while ((n > 0))
+	{
+		*(char *)dst ++ = *(char *)src ++;
+		n --;
+	}
+	return (dstcpy);
+}
