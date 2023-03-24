384af4285e589515d6115a82c5c4b3e72e544a34
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-24 23:58:19 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,27 +6,30 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:48:32 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/23 16:23:03 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/03/24 23:56:29 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+void	f()
+{
+	system("leaks a.out");
+}
+
 int	main(int ac, char **av)
 {
 	int		infile;
 	int		fd[2];
-	char	**args;
+	c_list	cmd1;
 
+	//atexit(f);
 	av ++;
-	infile = open(av[0], O_WRONLY);
-	if (infile == -1)
-		printf("bash: %s: No such file or directory\n", av[0]);
+	cmd1 = ft_lstnew(ft_split(av[1], ' '), open(av[0], O_WRONLY));
+	// infile = open(av[0], O_WRONLY);
+	// if (infile == -1)
+	// 	printf("bash: %s: No such file or directory\n", av[0]);
 	//args = ft_split(av[1], ' ');
-
-	while (*environ)
-	{
-		printf("%s\n", *environ);
-		environ ++;
-	}
+	err = execve("/sbin/ping", args, NULL);
+	printf("%s\n", );
 }
