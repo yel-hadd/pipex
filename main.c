47d19bf25fb4909fb966fbfc0c89ccaf53f1be71
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-25 00:46:46 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:48:32 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/24 23:56:29 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/03/25 00:03:29 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,15 @@ void	f()
 int	main(int ac, char **av)
 {
 	int		infile;
+	int		outfd;
 	int		fd[2];
 	c_list	cmd1;
 
-	//atexit(f);
+	pipe(fd);
+	outfd = dup2(fd[1], 1);
 	av ++;
 	cmd1 = ft_lstnew(ft_split(av[1], ' '), open(av[0], O_WRONLY));
+	//atexit(f);
 	// infile = open(av[0], O_WRONLY);
 	// if (infile == -1)
 	// 	printf("bash: %s: No such file or directory\n", av[0]);
