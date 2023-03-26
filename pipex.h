25052f5b14016da092f82dd8100ddc02a3aa762a
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-26 22:02:42 +0000

    Update pipex.h

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 15:13:41 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/24 23:53:45 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/03/26 20:36:59 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ typedef struct commande
 extern char **environ;
 
 char	**ft_split(char *s, char c);
-c_list	*ft_lstnew(char **args, int in);
+c_list	*ft_lstnew(char **args, int in, int out);
 void	*ft_memcpy(void *dst, void *src, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char *s1, char *s2, int c);
