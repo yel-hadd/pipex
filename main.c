24abad249222f70383be1bb1dd4aff8df83d221a
Yassine El Haddad <megahaddad15@gmail.com>
2023-03-29 21:21:48 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:48:32 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/28 01:41:06 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/03/29 21:19:46 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,76 +41,28 @@ void	ft_lstclear(c_list **lst)
 	}
 }
 
-void	print_error(int check)
-{
-	if (check == -1)
-		perror("Error");
-	else if (check == -2)
-		perror("Error");
-	else if (check == -3)
-		perror("Error");
-	else if (check == -4)
-		perror("Error");
-	exit(1);
-}
-
-int	is_valid(c_list **c)
-{
-	c_list	*node;
-	int		valid;
-
-	valid = 1;
-	node = *c;
-	if (node->args == NULL)
-		return (-1);
-	else if (node->path == NULL)
-		return (-2);
-	else if (node->in == -1)
-		return (-3);
-	else if (node->out == -1)
-		return (-4);
-	return (1);
-}
-
-int	execute(c_list **c)
-{
-	int		err;
-	c_list	*node;
-	int		check;
-
-	node = *c;
-	check = is_valid(c);
-	if (check != 1)
-		return (print_error(check), -1);
-	err = execve(node->path, node->args, NULL);
-	if (err == -1)
-		return (2);
-	return (0);
-}
-
 int	main(int ac, char **av)
 {
 	int		fd[2];
 	c_list	*cmd1;
 	c_list	*cmd2;
 	int		id;
-	int		k;
+	int		infile;
+	int		outfile;
 
-	(void)	ac;
-	atexit(f);
+	//atexit(f);
 	pipe(fd);
+	infile = open(av[0], O_RDWR, 0644);
+	outfile = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC , 0644);
 	av ++;
-	cmd1 = ft_lstnew(ft_split(av[1], ' '), open(av[0], O_RDWR, 0644), fd[1]);
-	cmd2 = ft_lstnew(ft_split(av[2], ' '), fd[0], open(av[3], O_WRONLY | O_CREAT | O_TRUNC , 0644));
+	cmd1 = ft_lstnew(ft_split(av[1], ' '), infile, fd[1]);
+	cmd2 = ft_lstnew(ft_split(av[2], ' '), fd[0], outfile);
 	id = fork();
 	if (id == 0)
 	{
 		close(fd[0]);
-		dup2(cmd1->in, 0);
-		dup2(cmd1->out, 1);
-		k = execute(&cmd1);
-		if (k == 0)
-			write(1, "ERROR\n", 6);
+		if (execute_node(&cmd1) == 0)
+			print_error("Error");
 		close(fd[1]);
 	}
 	wait(NULL);
@@ -118,11 +70,8 @@ int	main(int ac, char **av)
 	if (id == 0)
 	{
 		close(fd[1]);
-		dup2(fd[0], 0);
-		dup2(cmd2->out, 1);
-		k = execute(&cmd2);
-		if (k == 0)
-			write(1, "ERROR\n", 6);
+		if (execute_node(&cmd2) == 0)
+			print_error("Error");
 		close(fd[0]);
 	}
 	ft_lstclear(&cmd1);
