28a19192a866aded78eef23d7c609568887cef85
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-01 01:20:12 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,22 +6,21 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:48:32 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/01 00:26:49 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/01 01:16:08 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	f()
+int	run_node(t_list **c, int id)
 {
-	system("leaks pipex");
-}
+	t_list	*node;
 
-int	run_node(c_list **c)
-{
-	c_list	*node;
-
+	if (id != 0)
+		return (0);
 	node = *c;
+	if (node->close != -2)
+		close(node->close);
 	if (is_valid(c) == 1)
 	{
 		dup2(node->in, 0);
@@ -31,36 +30,39 @@ int	run_node(c_list **c)
 	return (-1);
 }
 
-
 int	main(int ac, char **av)
 {
 	int		fd[2];
-	c_list	*list;
-	c_list	*head;
+	t_list	*list;
+	t_list	*head;
+	t_list	*node;
 	int		id;
 
 	if (ac != 5)
-		return (print_error("Invalid Syntax: ./pipex file1 cmd1 cmd2 file2\n"), 1);
+		return ((int) write(1, "Invalid Args\n", 13));
 	pipe(fd);
-	list = ft_lstnew(ft_split(av[2], ' '), open(av[1], O_RDWR), fd[1]);
-	ft_lstadd_back(&list, ft_lstnew(ft_split(av[3], ' '), fd[0], open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC , 0644)));
+	list = ft_lstnew(ft_split(av[2], ' '), open(av[1], O_RDWR), fd[1], fd[0]);
+	node = ft_lstnew(ft_split(av[3], ' '), fd[0], \
+	open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0644), -2);
+	ft_lstadd_back(&list, node);
 	head = list;
 	id = fork();
-	if (id == 0)
-	{
-		close(fd[0]);
-		if (run_node(&list) == -1)
-			return (ft_lstclear(&head), print_error("Error"), 1);
-	}
+	if (run_node(&list, id) == -1)
+		return (ft_lstclear(&head), print_error("Error"), 1);
 	close(fd[1]);
 	id = fork();
-	if (id == 0)
-	{
-		if (run_node(&list->next) == -1)
-			return (ft_lstclear(&head), print_error("Error"), 1);
-	}
+	if (run_node(&list->next, id) == -1)
+		return (ft_lstclear(&head), print_error("Error"), 1);
 	close(fd[0]);
 	while (wait(NULL) != -1)
 		;
 	return (ft_lstclear(&head), 0);
 }
+/*
+pipex.h: Error!
+utils/execute_node.c: Error!
+utils/ft_calloc.c: Error!
+utils/ft_lstdelone.c: Error!
+utils/ft_lstnew.c: Error!
+utils/get_path.c: Error!
+*/
