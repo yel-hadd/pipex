d14f3b2e2e72a4cd89835f47b661cb2a2ee345b2
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-01 01:20:33 +0000

    Update ft_lstclear.c

--- utils/ft_lstclear.c
+++ utils/ft_lstclear.c
@@ -6,15 +6,15 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/30 16:47:26 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/30 16:49:14 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/01 01:16:08 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-void	ft_lstclear(c_list **lst)
+void	ft_lstclear(t_list **lst)
 {
-	c_list	*tmp;
+	t_list	*tmp;
 
 	if (!lst || !(*lst))
 		return ;
