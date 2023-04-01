209d86f958446a995e0f7ba352051242e2b8e6c1
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-01 01:20:46 +0000

    Update is_valid.c

--- utils/is_valid.c
+++ utils/is_valid.c
@@ -6,15 +6,15 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 20:41:42 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/29 21:48:16 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/01 01:16:08 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-int	is_valid(c_list **c)
+int	is_valid(t_list **c)
 {
-	c_list	*node;
+	t_list	*node;
 
 	node = *c;
 	if (node->args == NULL || node->path == NULL)
