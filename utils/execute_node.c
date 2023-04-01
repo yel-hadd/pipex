713993900afedc1e91dcfa5c7e4a838edf385786
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-01 01:20:25 +0000

    Update execute_node.c

--- utils/execute_node.c
+++ utils/execute_node.c
@@ -6,19 +6,19 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 20:40:56 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/29 21:28:39 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/01 01:17:37 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-int	execute_node(c_list **c)
+int	execute_node(t_list **c)
 {
 	int		err;
-	c_list	*node;
+	t_list	*node;
 
 	node = *c;
-    dup2(node->in, 0);
+	dup2(node->in, 0);
 	dup2(node->out, 1);
 	if (is_valid(c) != 1)
 		return (print_error("Error"), -1);
@@ -27,4 +27,3 @@ int	execute_node(c_list **c)
 		return (2);
 	return (0);
 }
-
