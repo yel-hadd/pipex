5a8e33fc6e559f361b454afbbb913ac3ec5bb6a3
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-01 01:20:43 +0000

    Update get_path.c

--- utils/get_path.c
+++ utils/get_path.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 21:55:54 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/03/29 20:27:16 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/01 01:19:21 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*get_path(char *cmd)
 	{
 		tmp = dirs[i];
 		dirs[i] = ft_strjoin(dirs[i], cmd, 2);
-		if((access(dirs[i], F_OK) == 0) && (access(dirs[i], X_OK) != -1))
+		if ((access(dirs[i], F_OK) == 0) && (access(dirs[i], X_OK) != -1))
 		{
 			res = ft_strdup(dirs[i]);
 			return (free(tmp), free_2d(dirs), res);
