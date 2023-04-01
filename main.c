6c8b81374f2b206f336397d6e33eedf997caf602
Yassine El Haddad <megahaddad15@gmail.com>
2023-04-01 00:29:43 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:48:32 by yel-hadd          #+#    #+#             */
-/*   Updated: 2023/04/01 00:01:49 by yel-hadd         ###   ########.fr       */
+/*   Updated: 2023/04/01 00:26:49 by yel-hadd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,43 +31,36 @@ int	run_node(c_list **c)
 	return (-1);
 }
 
+
 int	main(int ac, char **av)
 {
 	int		fd[2];
 	c_list	*list;
+	c_list	*head;
 	int		id;
 
 	if (ac != 5)
-	{
-		print_error("Invalid Syntax: ./pipex file1 cmd1 cmd2 file2\n");
-		return (0);
-	}
+		return (print_error("Invalid Syntax: ./pipex file1 cmd1 cmd2 file2\n"), 1);
 	pipe(fd);
 	list = ft_lstnew(ft_split(av[2], ' '), open(av[1], O_RDWR), fd[1]);
 	ft_lstadd_back(&list, ft_lstnew(ft_split(av[3], ' '), fd[0], open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC , 0644)));
+	head = list;
 	id = fork();
 	if (id == 0)
 	{
 		close(fd[0]);
 		if (run_node(&list) == -1)
-		{
-			print_error("Error");
-			return (1);
-		}
+			return (ft_lstclear(&head), print_error("Error"), 1);
 	}
 	close(fd[1]);
-	if (id != 0)
-		id = fork();
+	id = fork();
 	if (id == 0)
 	{
 		if (run_node(&list->next) == -1)
-		{
-			print_error("Error");
-			return (1);
-		}
+			return (ft_lstclear(&head), print_error("Error"), 1);
 	}
 	close(fd[0]);
-	while (wait(NULL) != -1) ;
-	ft_lstclear(&list);
-	exit(0);
+	while (wait(NULL) != -1)
+		;
+	return (ft_lstclear(&head), 0);
 }
